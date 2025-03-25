1b51a338a58abc668e9c75233f03ac591983a6a2
Segundo Ferreira <segundo.ferreira28@gmail.com>
2025-03-25 20:37:54 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+ 
+  
+  const quien = ['Las personas', 'La comida', 'Los paisajes'];
+  const porque = ['son hermosos/as', 'es increible', 'experiencia inolvidable'];
+  const lugares = ['en Buenos Aires', 'en Mendoza', 'en La patagonia'];
+  
+const randomIndexQuien = Math.floor(Math.random() * quien.length );
+const randomIndexPorque = Math.floor(Math.random() * porque.length );
+const randomIndexLugares = Math.floor(Math.random() * lugares.length );
+
+const excuses = quien[randomIndexQuien] + " " + porque[randomIndexPorque] + " " + lugares [randomIndexLugares];
+
+
+const myExcuse = document.getElementById('myExcuse');
+
+ myExcuse.innerHTML = excuses
+  
 };
